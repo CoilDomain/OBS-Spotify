defed8d885836e24c908c38e3d9e7a1245ac2b7d
CoilDomain <xorphenochx@gmail.com>
2021-09-20 19:35:41 -0400

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/coildomain/OBS-Spotify
+
+go 1.13
+
+require (
+	github.com/getlantern/golog v0.0.0-20190830074920-4ef2e798c2d7
+	github.com/getlantern/systray v1.1.0
+	github.com/mitchellh/go-ps v1.0.0 // indirect
+	github.com/skratchdot/open-golang v0.0.0-20200116055534-eef842397966 // indirect
+	golang.org/x/sys v0.0.0-20200515095857-1151b9dac4a9
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"os/user"
+	"path/filepath"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/getlantern/systray"
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
+)
+
+func main() {
+	systray.Run(onReady, onExit)
+}
+
+func fileExists(file string) bool {
+	info, err := os.Stat(file)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func transformEncoding(rawReader io.Reader, trans transform.Transformer) (string, error) {
+	ret, err := ioutil.ReadAll(transform.NewReader(rawReader, trans))
+	if err == nil {
+		return string(ret), nil
+	} else {
+		return "", err
+	}
+}
+
+func FromShiftJIS(str string) (string, error) {
+	return transformEncoding(strings.NewReader(str), japanese.ShiftJIS.NewDecoder())
+}
+
+var usr, _ = user.Current()
+var path = usr.HomeDir
+var filename = "obs-currentsong.txt"
+var logfilepath = filepath.Join(path, filename)
+
+func onReady() {
+	systray.SetIcon(Red)
+	systray.SetTitle("OBS Current Song")
+	systray.SetTooltip("OBS Current Song")
+	mQuit := systray.AddMenuItem("Quit", "Quit")
+	go func() {
+		<-mQuit.ClickedCh
+		systray.Quit()
+	}()
+
+	for {
+		cmd := exec.Command("powershell.exe", "(get-process -processname Spotify | select -unique -property MainWindowTitle).MainWindowTitle")
+		cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+		out, _ := cmd.Output()
+		currentsong := string(out)
+		convertsongtitletoutf8, _ := FromShiftJIS(currentsong)
+		fmt.Println(convertsongtitletoutf8)
+
+		if convertsongtitletoutf8 != "" {
+			systray.SetIcon(Green)
+			logfile, err := os.Create(logfilepath)
+			defer logfile.Close()
+			w := bufio.NewWriter(logfile)
+			_, err = fmt.Fprintf(w, "%s", convertsongtitletoutf8)
+			check(err)
+			w.Flush()
+		} else {
+			systray.SetIcon(Red)
+		}
+		time.Sleep(time.Second)
+	}
+}
+
+func onExit() {
+	// clean up here
+}
